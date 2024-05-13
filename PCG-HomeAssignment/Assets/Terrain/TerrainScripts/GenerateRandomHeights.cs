e316f67ca4a2f2bba940d7d4c0d5fd8119264441
MatthiasSaliba <146810236+MatthiasSaliba@users.noreply.github.com>
2024-05-13 17:40:32 +0200

    Added Trees

--- PCG-HomeAssignment/Assets/Terrain/TerrainScripts/GenerateRandomHeights.cs
+++ PCG-HomeAssignment/Assets/Terrain/TerrainScripts/GenerateRandomHeights.cs
@@ -13,6 +13,14 @@ public class TerrainTextureData
     public float maxHeight;
 }
 
+[System.Serializable]
+public class TreeData
+{
+    public GameObject treeMesh;
+    public float minHeight;
+    public float maxHeight;
+}
+
 public class GenerateRandomHeights : MonoBehaviour
 {
     
@@ -52,6 +60,21 @@ public class GenerateRandomHeights : MonoBehaviour
     [SerializeField] 
     private float terrainTextureBlendOffset = 0.01f;
     
+    [Header("Tree Data")]
+    [SerializeField]
+    private List<TreeData> treeData;
+
+    [SerializeField] 
+    private int maxTrees = 2000;
+
+    [SerializeField] 
+    private int treeSpacing = 10;
+    
+    [SerializeField]
+    private bool addTrees = false;
+
+    [SerializeField]
+    private int terrainLayerIndex;
     
     
     // Start is called before the first frame update
@@ -68,6 +91,7 @@ public class GenerateRandomHeights : MonoBehaviour
         
         GenerateHeights();
         AddTerrainTextures();
+        AddTrees();
     }
 
     void GenerateHeights()
@@ -175,6 +199,70 @@ public class GenerateRandomHeights : MonoBehaviour
         for (int i = 0; i < alphamap.Length; i++)
         {
             alphamap[i] = alphamap[i] / total;
+        }
+    }
+    
+    private void AddTrees()
+    {
+        TreePrototype[] trees = new TreePrototype[treeData.Count];
+        
+        for (int i = 0; i < treeData.Count; i++)
+        {
+            trees[i] = new TreePrototype();
+            trees[i].prefab = treeData[i].treeMesh;
+        }
+        
+        terrainData.treePrototypes = trees;
+
+        List<TreeInstance> treeInstanceList = new List<TreeInstance>();
+
+        if (addTrees)
+        {
+            for (int z = 0; z < terrainData.size.z; z += treeSpacing)
+            {
+                for (int x = 0; x < terrainData.size.x; x += treeSpacing)
+                {
+                    for (int treeIndex = 0; treeIndex < trees.Length; treeIndex++)
+                    {
+                        if (treeInstanceList.Count < maxTrees)
+                        {
+                            float currentHeight = terrainData.GetHeight(x, z) / terrainData.size.y;
+
+                            if (currentHeight >= treeData[treeIndex].minHeight &&
+                                currentHeight <= treeData[treeIndex].maxHeight)
+                            {
+                                float randomX = (x + Random.Range(-5.0f, 5.0f)) / terrainData.size.x;
+                                float randomZ = (z + Random.Range(-5.0f, 5.0f)) / terrainData.size.z;
+                                
+                                Vector3 treePosition = new Vector3(randomX * terrainData.size.x, 
+                                                                    currentHeight * terrainData.size.y, 
+                                                                    randomZ * terrainData.size.z) + this.transform.position;
+                                RaycastHit raycastHit;
+
+                                int layerMask = 1 << terrainLayerIndex;
+                                
+                                if(Physics.Raycast(treePosition, -Vector3.up, out raycastHit, 100, layerMask) || 
+                                    Physics.Raycast(treePosition, Vector3.up, out raycastHit, 100, layerMask))
+                                {
+                                    float treeDistance = (raycastHit.point.y - this.transform.position.y) / terrainData.size.y;
+                                    
+                                    TreeInstance treeInstance = new TreeInstance();
+                                    treeInstance.position = new Vector3(randomX, treeDistance, randomZ);
+                                    treeInstance.rotation = Random.Range(0, 360);
+                                    treeInstance.prototypeIndex = treeIndex;
+                                    treeInstance.color = Color.white;
+                                    treeInstance.lightmapColor = Color.white;
+                                    treeInstance.heightScale = 0.95f;
+                                    treeInstance.widthScale = 0.95f;
+
+                                    treeInstanceList.Add(treeInstance);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            terrainData.treeInstances = treeInstanceList.ToArray();
         }
     }
 
